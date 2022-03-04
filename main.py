89299c163e0e7e780a19694b52986e94d9d9a72a
waffle <b07902011@ntu.edu.tw>
2022-03-04 13:16:56 +0800

    feat: echo message

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI,Request
+import telegram 
+from telegram.ext import Dispatcher, MessageHandler, Filters, CallbackContext
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+app=FastAPI()
+
+bot = telegram.Bot(token=(config['TELEGRAM']['ACCESS_TOKEN']))
+
+@app.post('/hook')
+async def webhook_handler(request:Request):
+    """Set route /hook with POST method will trigger this method."""
+    if request.method == "POST":
+        body = await request.json()
+        update = telegram.Update.de_json(body, bot)
+
+        # Update dispatcher process that handler to process this message
+        dispatcher.process_update(update)
+    return 'ok'
+
+def reply_handler(update:telegram.Update, context: CallbackContext):
+    """Reply message."""
+    text = update.message.text
+    update.message.reply_text(text)
+
+dispatcher = Dispatcher(bot, None)
+
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
